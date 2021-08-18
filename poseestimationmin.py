2227802d880fcf305bcc4d79b4ac34138e405301
kimsezin <48678553+kimsezin@users.noreply.github.com>
2021-08-18 23:50:32 +0900

    poseestimation 스켈레톤 이미지 저장

--- poseestimationmin.py
+++ poseestimationmin.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
-
+import numpy as np
 mpDraw=mp.solutions.drawing_utils
 #포즈를 감지하여 여기에 쓸것임.
 mpPose=mp.solutions.pose
@@ -10,14 +10,20 @@ pose=mpPose.Pose()
 
 
 cap = cv2.VideoCapture('1.mp4')
+index = 0
+
 pTime=0
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
+    black_img = np.zeros((640, 480, 3), dtype=np.uint8)
     # print(results.pose_landmarks) #결과를 확인 x,y,z좌표 랜드마크를 확인
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img,results.pose_landmarks,mpPose.POSE_CONNECTIONS) #이미지의 좌표에 점을 생성,라인생성
+        mpDraw.draw_landmarks(black_img,results.pose_landmarks,mpPose.POSE_CONNECTIONS)
+        cv2.imwrite('Result/image%d.jpg'%index,black_img)
+        index=index+1
         #내가 원하는 랜드마크 번호 5를 원하면
         for id, lm in enumerate(results.pose_landmarks.landmark):
             h,w,c = img.shape
