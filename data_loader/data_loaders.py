ecab3833acdc6877d7bbed90c10e2c7cba913564
nudago <nayohan13@gmail.com>
2021-09-07 14:48:56 +0000

    [ADD] 스켈레톤 커스텀 데이터로더 구현

---
+++ data_loader/data_loaders.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+from datasets import SkeletonDataset
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+
+
+class SkeletonDataLoader():
+    def __init__(self, data_dir, batch_size, shffule=True, validation_split=0.1, num_workers=1, trsfm=None):
+        self.columns = ["head", "shoulder", "elbow", "hand", "hip", "foot", "elbow_angle", "hip_angle", "knee_angle", "image_path", "label"]
+
+        self.data_dir = data_dir
+        self.train_dir = os.path.join(self.data_dir, 'train')
+        self.eval_dir = os.path.join(self.data_dir, 'eval')
+
+        csv_path=os.path.join(self.train_dir, "train.csv")
+        self.df = pd.read_csv(csv_path, names=self.columns)
+
+        self.dataset = SkeletonDataset(self.df)
+        self.train_dataset, self.valid_dataset = train_test_split(self.dataset, test_size=validation_split, random_state=42, stratify=self.df.to_numpy()[:,-1])
+
+        self.train_dataloader = DataLoader(self.train_dataset, batch_size, num_workers, pin_memory=True)
+        self.valid_dataloader = DataLoader(self.valid_dataset, batch_size, num_workers, pin_memory=True)
+
+        #assert trsfm is not None, "Set the transform on train set"
+
+    def split_validation(self):
+        return self.train_dataloader, self.valid_dataloader
+
+if __name__ == "__main__":
+    dataloader = SkeletonDataLoader("./", batch_size=16, validation_split=0.2, num_workers=8)
+    train_dataloader, vliad_dataloader = dataloader.split_validation()
+    for data in train_dataloader:
+        print(data)
---
+++ data_loader/datasets.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+
+class SkeletonDataset(Dataset):
+    def __init__(self, df, transform=None):
+        super().__init__()
+        #assert transform is not None, "Set the transform on train set"
+        self.transform = transform
+        self.df = df
+
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, index):
+        label = torch.tensor(self.df['label'].iloc[index])
+        image = Image.open(self.df['image_path'].iloc[index])
+        image = np.array(image.convert("RGB"))
+        if self.transform:
+            transformed_image = self.transform(image=image)['image']
+            image = transformed_image['image']
+        return image, label
