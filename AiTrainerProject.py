cb1a935bb072a261929ee3a72eff05258472f687
qudrnr217 <dksk1233@naver.com>
2021-09-09 00:01:45 +0900

    AiTrainerProject.py 실시간 label 출력

--- AiTrainerProject.py
+++ AiTrainerProject.py
@@ -1,3 +1,4 @@
+from classification_model.SupervisedLearning import classification
 from math import inf
 import cv2
 import numpy as np
@@ -83,6 +84,7 @@ while True:
             foot = (lmList[28][2])
 
         keypoint = [head, shoulder, elbow, hand, hip, foot, int(elbow_angle), int(hip_angle),int(knee_angle)]  #CSV생성용 키포인트 데이터 생성
+        classification(keypoint)
         keypoint_list.append(keypoint) 
         #k_max, k_min = max(keypoint), min(keypoint)  #최소값, 최대값 이용하지않고 sholder - hand간 거리로 자세 레이블링
         #answer = defineLabel(keypoint, k_max, k_min)   #레이블 구분 함수 (0,1,2)리턴
@@ -91,8 +93,8 @@ while True:
         # 사전에 입력한 시작점과 끝점 외의 준비자세는 레이블을 0으로 둠
         answer = defineLabel(elbow_angle,hip_angle,knee_angle, int(cap.get(cv2.CAP_PROP_POS_FRAMES)), start_sec, end_sec)
         label_list.append([index, answer]) # index별로 뽑기위해 keypoint 리스트에 추가
-
-        print(hip_angle,elbow_angle,knee_angle)
+        
+        # print(hip_angle,elbow_angle,knee_angle)
         #check for the push up curls
         color = (255,0,255)
         if per==100:
@@ -131,6 +133,6 @@ while True:
 #print(f"final_max: {final_max}")
 #print(f"final_min: {final_min}")
 
-print(label_list)
+# print(label_list)
 writecsv = WriteCSV('./train/', label_list, keypoint_list, video_name)
 writecsv.merge_train_csv()
--- classification_model/SupervisedLearning.py
+++ classification_model/SupervisedLearning.py
@@ -5,10 +5,11 @@ from sklearn.metrics import classification_report
 
 
 class classification():
-    def __init__(self):
+    def __init__(self,keypoint):
 #데이터 전처리 및 가공
-        df_train=pd.read_csv("./train/test.csv", names=["head","shoulder","elbow","hand","hip","foot","angle","path","label"]) #학습 데이터들
-        df_test=pd.read_csv("./train/train.csv",names=["head","shoulder","elbow","hand","hip","foot","angle","path","label"]) #실시간 데이터
+        self.keypoint=[keypoint]
+        df_train=pd.read_csv("./train/test.csv", names=["head","shoulder","elbow","hand","hip","foot","elbow_angle","hip_angle","knee_angle","path","label"]) #학습 데이터들
+        df_test=pd.read_csv("./train/train.csv",names=["head","shoulder","elbow","hand","hip","foot","elbow_angle","hip_angle","knee_angle","path","label"]) #실시간 데이터
 
         #train 데이터
         x=df_train.drop(columns=['path','label'])
@@ -21,26 +22,31 @@ class classification():
         # #데이터 전처리(분할)
         x_train, x_valid, y_train, y_valid = train_test_split(x,y, test_size=0.3,random_state=2020)
 
-        print(x_train.shape)
-        print(x_test.shape)
-        print(y_train.shape)
-        print(y_test.shape)
+        one_line_data=self.keypoint
+        # print(x_train.shape)
+        # print(x_test.shape)
+        # print(y_train.shape)
+        # print(y_test.shape)
 
         # #학습 
         model=DecisionTreeClassifier()
         model.fit(x_train,y_train)
 
         # #평가
-        y_train_pred=model.predict(x_train)
-        y_test_pred=model.predict(x_test)
-        y_valid_pred=model.predict(x_valid)
-        y_test_pred=model.predict(x_test)
+        # y_train_pred=model.predict(x_train)
+        # y_test_pred=model.predict(x_test)
+        # y_valid_pred=model.predict(x_valid)
+        # y_test_pred=model.predict(x_test)
 
-        print(classification_report(y_train,y_train_pred))
-        print(classification_report(y_valid,y_valid_pred))
-        # print(y_test_pred)
-        print(classification_report(y_test,y_test_pred))
+        one_line_pred=model.predict(one_line_data)
+        print(one_line_pred)
+
+        # print(classification_report(y_train,y_train_pred))
+        # print(classification_report(y_valid,y_valid_pred))
+        # # print(y_test_pred)
+        # print(classification_report(y_test,y_test_pred))
         # print(classification_report(y_train,y_test_pred))
 
 if __name__ == "__main__":
-    classification()
+    key_point_data=[292,269,377,487,317,450,164,161,197]
+    classification(key_point_data)
