4225b7a9f2d5e18fbee9b0a99f42501adc360c0a
BumBamBi <dlrhkddn0325@gmail.com>
2021-10-05 15:32:25 +0900

    게이지 바 원형으로 변경 및 카운트 위치 이동

--- AiTrainerProject.py
+++ AiTrainerProject.py
@@ -128,19 +128,22 @@ while True:
         #print(f"count: {count}")
         #print(f"index: {index}")
         #draw bar
-        cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
-        cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
-        cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4, color, 4)
-
+        #cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
+        #cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
+        #cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4, color, 4)
+        
+        
+        elif(per != 0):
+            img = cv2.ellipse(img, (1100,600), (90,90), 270, 0, per*3.6, (255, 190, 0), 15, 2)
+        
         #draw curl count
-        cv2.rectangle(img, (0,450), (250,720),(0,255,0),cv2.FILLED)
-        cv2.putText(img, str(int(count)), (45,670), cv2.FONT_HERSHEY_PLAIN, 15, (255, 0, 0) ,25)
+        #cv2.rectangle(img, (0,450), (250,720),(0,255,0),cv2.FILLED)
+        cv2.putText(img, str(int(count)), (1053,650), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 15)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-    cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5,
-                (255, 0, 0), 5)
+    #cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
     cv2.imshow("Image",img)
     cv2.waitKey(1)
 
