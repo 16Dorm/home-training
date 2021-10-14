c5fb23014fab7f9b4c3eb2f3084793c20e699638
nudago <nayohan13@gmail.com>
2021-10-14 21:38:19 +0900

    [ADD] 동영상전체에대해 학습데이터 생성 + 저장경로변경

--- AiTrainerProject.py
+++ AiTrainerProject.py
@@ -8,160 +8,166 @@ from utils.add_Pictogram import add_Pictogram
 from utils.defineLabel import defineLabel
 from utils.WriteCSV import WriteCSV
 
+def run_pose_estimation(video_name):
+    cap = cv2.VideoCapture("./Video/" + video_name + ".mp4")
+    #cap2=cv2.VideoCapture(0) #카메라 번호
 
-# 영상 선택 및 실행
-f = open("video_name.txt", 'r')
-video_name = f.read()
-cap = cv2.VideoCapture("./Video/" + video_name + ".mp4")
-cap2=cv2.VideoCapture(0) #카메라 번호
+    # 사전 준비시간을 label0으로 잘라내기 위한 작업
+    with open('video_list.txt', 'r') as infile:
+        data = infile.readlines()
+        for i in data:
+            v_name, start_sec, end_sec = (i.split())
+            if (video_name == v_name):
+                start_sec = int(start_sec)
+                end_sec = int(end_sec)
+                if (end_sec == 0):
+                    end_sec = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                break
 
-# 사전 준비시간을 label0으로 잘라내기 위한 작업
-with open('video_list.txt', 'r') as infile:
-    data = infile.readlines()
-    for i in data:
-        v_name, start_sec, end_sec = (i.split())
-        if (video_name == v_name):
-            start_sec = int(start_sec)
-            end_sec = int(end_sec)
-            if (end_sec == 0):
-                end_sec = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    detector =pm.poseDetector(video_name)
+    count = 0
+    dir = 0
+    pTime = 0
+    index = 0
+    label_list = []
+    keypoint_list = []
+    #class_var=classification()
+    #class_var.train_csv()
+    while True:
+        success, img =cap.read()
+        if not success:
             break
-
-detector =pm.poseDetector()
-count = 0
-dir = 0
-pTime = 0
-index = 0
-label_list = []
-keypoint_list = []
-#class_var=classification()
-#class_var.train_csv()
-while True:
-    success, img =cap.read()
-    if not success:
-        break
-    img = cv2.resize(img, (1280,720)) #영상의 크기 조절, 프레임 조절할 수 있다
-    black_img = np.zeros((480, 640, 3), dtype=np.uint8) ##데이터 저장용 검은배경 이미지 생성
-    # img = cv2.imread("2.PNG")  # 각도를 얻기 위한 이미지 각도를 얻고 주석
-    # 이후에 할일은 포즈 모듈을 가져와야함 포즈 모듈로 각도 재기
-    
-    img = detector.findPose(img, black_img, index, False) #false를 해서 우리가 보고자하는 점 외에는 다 제거
-    index+=1
-    lmList = detector.findPosition(img, False) #그리기를 원하지 않으므로 false
-    per = 0
-    # print(lmList) #좌표를 프린트
-    keypoint = [] # 핵심 키포인트를 담을 리스트
-    if len(lmList)!=0:
-        # print(lmList[24][1])  # 24번은 엉덩이 x축 좌표만
-        # print(lmList[1][1]) #1번은 눈을 표시 x축 좌표만
+        img = cv2.resize(img, (1280,720)) #영상의 크기 조절, 프레임 조절할 수 있다
+        black_img = np.zeros((480, 640, 3), dtype=np.uint8) ##데이터 저장용 검은배경 이미지 생성
+        # img = cv2.imread("2.PNG")  # 각도를 얻기 위한 이미지 각도를 얻고 주석
+        # 이후에 할일은 포즈 모듈을 가져와야함 포즈 모듈로 각도 재기
         
-        # Right Arm
-        if lmList[24][1]<lmList[1][1]:
-            elbow_angle = detector.findAngle(img, 12,14,16)
-            if (elbow_angle > 180):
-                elbow_angle  = 360 - elbow_angle
-            hip_angle= detector.findAngle(img,12,24,26)
-            knee_angle= detector.findAngle(img,24,26,28)
+        img = detector.findPose(img, black_img, index, False) #false를 해서 우리가 보고자하는 점 외에는 다 제거
+        index+=1
+        lmList = detector.findPosition(img, False) #그리기를 원하지 않으므로 false
+        per = 0
+        # print(lmList) #좌표를 프린트
+        keypoint = [] # 핵심 키포인트를 담을 리스트
+        if len(lmList)!=0:
+            # print(lmList[24][1])  # 24번은 엉덩이 x축 좌표만
+            # print(lmList[1][1]) #1번은 눈을 표시 x축 좌표만
+            
+            # Right Arm
+            if lmList[24][1]<lmList[1][1]:
+                elbow_angle = detector.findAngle(img, 12,14,16)
+                if (elbow_angle > 180):
+                    elbow_angle  = 360 - elbow_angle
+                hip_angle= detector.findAngle(img,12,24,26)
+                knee_angle= detector.findAngle(img,24,26,28)
 
-            # 그리기
-            detector.drawPoint(img, 12, 14, 16, 'elbow', elbow_angle)
-            detector.drawPoint(img, 12, 24, 26, 'hip', hip_angle)
-            detector.drawPoint(img, 24, 26, 28, 'knee', knee_angle)
+                # 그리기
+                detector.drawPoint(img, 12, 14, 16, 'elbow', elbow_angle)
+                detector.drawPoint(img, 12, 24, 26, 'hip', hip_angle)
+                detector.drawPoint(img, 24, 26, 28, 'knee', knee_angle)
 
-            # 각도를 퍼센트로 나타내는 코드
-            per = np.interp(elbow_angle, (80, 160), (100, 0))
-            # print(angle, per)
-            bar = np.interp(elbow_angle, (80, 160), (650, 100))  # 앞에가 최소 뒤에가 최대
-            head = lmList[0][2]
-            shoulder = (lmList[11][2])
-            elbow = (lmList[13][2])
-            hand = (lmList[15][2])
-            hip = (lmList[23][2])
-            foot = (lmList[27][2])
-        
-        # Left Arm
-        else:
-            elbow_angle=detector.findAngle(img, 11, 13, 15)
-            if (elbow_angle > 180):
-                elbow_angle  = 360 - elbow_angle
-            hip_angle= detector.findAngle(img,11,23,25)
-            knee_angle= detector.findAngle(img,23,25,27)
+                # 각도를 퍼센트로 나타내는 코드
+                per = np.interp(elbow_angle, (80, 160), (100, 0))
+                # print(angle, per)
+                bar = np.interp(elbow_angle, (80, 160), (650, 100))  # 앞에가 최소 뒤에가 최대
+                head = lmList[0][2]
+                shoulder = (lmList[11][2])
+                elbow = (lmList[13][2])
+                hand = (lmList[15][2])
+                hip = (lmList[23][2])
+                foot = (lmList[27][2])
+            
+            # Left Arm
+            else:
+                elbow_angle=detector.findAngle(img, 11, 13, 15)
+                if (elbow_angle > 180):
+                    elbow_angle  = 360 - elbow_angle
+                hip_angle= detector.findAngle(img,11,23,25)
+                knee_angle= detector.findAngle(img,23,25,27)
 
-            # 그리기
-            detector.drawPoint(img, 11, 13, 15, 'elbow', elbow_angle)
-            detector.drawPoint(img, 11, 23, 25, 'hip', hip_angle)
-            detector.drawPoint(img, 23, 25, 27, 'knee', knee_angle)
+                # 그리기
+                detector.drawPoint(img, 11, 13, 15, 'elbow', elbow_angle)
+                detector.drawPoint(img, 11, 23, 25, 'hip', hip_angle)
+                detector.drawPoint(img, 23, 25, 27, 'knee', knee_angle)
 
-            # 각도를 퍼센트로 나타내는 코드
-            per = np.interp(elbow_angle, (80, 160), (100, 0))
-            # print(angle, per)
-            bar = np.interp(elbow_angle, (80, 160), (650, 100))  # 앞에가 최소 뒤에가 최대
-            head = (lmList[0][2])
-            shoulder = (lmList[12][2])
-            elbow = (lmList[14][2])
-            hand = (lmList[16][2])
-            hip = (lmList[24][2])
-            foot = (lmList[28][2])
+                # 각도를 퍼센트로 나타내는 코드
+                per = np.interp(elbow_angle, (80, 160), (100, 0))
+                # print(angle, per)
+                bar = np.interp(elbow_angle, (80, 160), (650, 100))  # 앞에가 최소 뒤에가 최대
+                head = (lmList[0][2])
+                shoulder = (lmList[12][2])
+                elbow = (lmList[14][2])
+                hand = (lmList[16][2])
+                hip = (lmList[24][2])
+                foot = (lmList[28][2])
 
-        keypoint = [head, shoulder, elbow, hand, hip, foot, int(elbow_angle), int(hip_angle),int(knee_angle)]  #CSV생성용 키포인트 데이터 생성
-        #class_var.keypoint_pred(keypoint)
-        keypoint_list.append(keypoint) 
-        #k_max, k_min = max(keypoint), min(keypoint)  #최소값, 최대값 이용하지않고 sholder - hand간 거리로 자세 레이블링
-        #answer = defineLabel(keypoint, k_max, k_min)   #레이블 구분 함수 (0,1,2)리턴
-        #keypoint = [shoulder,hand] 
+            keypoint = [head, shoulder, elbow, hand, hip, foot, int(elbow_angle), int(hip_angle),int(knee_angle)]  #CSV생성용 키포인트 데이터 생성
+            #class_var.keypoint_pred(keypoint)
+            keypoint_list.append(keypoint) 
+            #k_max, k_min = max(keypoint), min(keypoint)  #최소값, 최대값 이용하지않고 sholder - hand간 거리로 자세 레이블링
+            #answer = defineLabel(keypoint, k_max, k_min)   #레이블 구분 함수 (0,1,2)리턴
+            #keypoint = [shoulder,hand] 
 
-        # 사전에 입력한 시작점과 끝점 외의 준비자세는 레이블을 0으로 둠
-        answer = defineLabel(int(elbow_angle), int(hip_angle), int(knee_angle), int(cap.get(cv2.CAP_PROP_POS_FRAMES)), int(start_sec), int(end_sec))
-        label_list.append([index, answer]) # index별로 뽑기위해 keypoint 리스트에 추가
-        
-        # print(hip_angle,elbow_angle,knee_angle)
-        #check for the push up curls
-        color = (255,0,255)
-        if per==100:
-            color = (0, 255, 0)
-            if dir ==0: #올라가고있다.
-                count += 0.5
-                dir = 1
-        if per == 0:
-            color = (0, 255, 0)
-            if dir==1:
-                count += 0.5
-                dir = 0
-        #print(f"count: {count}")
-        #print(f"index: {index}")
+            # 사전에 입력한 시작점과 끝점 외의 준비자세는 레이블을 0으로 둠
+            answer = defineLabel(int(elbow_angle), int(hip_angle), int(knee_angle), int(cap.get(cv2.CAP_PROP_POS_FRAMES)), int(start_sec), int(end_sec))
+            label_list.append([index, answer]) # index별로 뽑기위해 keypoint 리스트에 추가
+            
+            # print(hip_angle,elbow_angle,knee_angle)
+            #check for the push up curls
+            color = (255,0,255)
+            if per==100:
+                color = (0, 255, 0)
+                if dir ==0: #올라가고있다.
+                    count += 0.5
+                    dir = 1
+            if per == 0:
+                color = (0, 255, 0)
+                if dir==1:
+                    count += 0.5
+                    dir = 0
+            #print(f"count: {count}")
+            #print(f"index: {index}")
 
-        #draw bar
-        if(per == 100):
-            img = cv2.ellipse(img, (1100,600), (90,90), 270, 0, per*3.6, (150, 250, 0), 15, 2)
-        elif(per != 0):
-            img = cv2.ellipse(img, (1100,600), (90,90), 270, 0, per*3.6, (255, 190, 0), 15, 2)
-        
-        #draw curl count
-        if(int(count) < 10):
-            cv2.putText(img, str(int(count)), (1053,650), cv2.FONT_HERSHEY_PLAIN, 10, (180, 50, 50), 15)
-        else:
-            cv2.putText(img, str(int(count)), (1020,640), cv2.FONT_HERSHEY_PLAIN, 8, (180, 50, 50), 15)
+            #draw bar
+            if(per == 100):
+                img = cv2.ellipse(img, (1100,600), (90,90), 270, 0, per*3.6, (150, 250, 0), 15, 2)
+            elif(per != 0):
+                img = cv2.ellipse(img, (1100,600), (90,90), 270, 0, per*3.6, (255, 190, 0), 15, 2)
+            
+            #draw curl count
+            if(int(count) < 10):
+                cv2.putText(img, str(int(count)), (1053,650), cv2.FONT_HERSHEY_PLAIN, 10, (180, 50, 50), 15)
+            else:
+                cv2.putText(img, str(int(count)), (1020,640), cv2.FONT_HERSHEY_PLAIN, 8, (180, 50, 50), 15)
 
-        #draw count bar
-        if(int(count) != 0):
-            img = cv2.ellipse(img, (1100,600), (105,105), 270, 0, int(count)*36, (90, 90, 255), 10, 2) 
-        elif(int(count) >= 10):
-            img = cv2.ellipse(img, (1100,600), (105,105), 270, 0, int(count)*36, (30, 30, 255), 10, 2)
+            #draw count bar
+            if(int(count) != 0):
+                img = cv2.ellipse(img, (1100,600), (105,105), 270, 0, int(count)*36, (90, 90, 255), 10, 2) 
+            elif(int(count) >= 10):
+                img = cv2.ellipse(img, (1100,600), (105,105), 270, 0, int(count)*36, (30, 30, 255), 10, 2)
 
 
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
-    img = add_Pictogram(img, int(per/34))
-    cv2.imshow("Image",img)
-    cv2.waitKey(1)
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5)
+        img = add_Pictogram(img, int(per/34))
+        cv2.imshow("Image",img)
+        cv2.waitKey(1) 
 
-#final_max=[max(head),max(shoulder),max(elbow),max(hand),max(hip),max(foot)]
-#final_min=[min(head),min(shoulder),min(elbow),min(hand),min(hip),min(foot)]
-#print(f"final_max: {final_max}")
-#print(f"final_min: {final_min}")
+    #final_max=[max(head),max(shoulder),max(elbow),max(hand),max(hip),max(foot)]
+    #final_min=[min(head),min(shoulder),min(elbow),min(hand),min(hip),min(foot)]
+    #print(f"final_max: {final_max}")
+    #print(f"final_min: {final_min}")
 
-# print(label_list)
-writecsv = WriteCSV('./train/', label_list, keypoint_list, video_name)
-writecsv.merge_train_csv()
+    # print(label_list)
+    writecsv = WriteCSV('./dataset/train/', label_list, keypoint_list, video_name)
+    writecsv.merge_train_csv()
+
+if __name__=="__main__":
+    f = open("video_name.txt", 'r')
+
+    video_name = f.readline().rstrip()
+    while video_name: # 여러동영상에 대해 학습데이터 생성
+        print(video_name)
+        run_pose_estimation(video_name)
+        video_name = f.readline().rstrip()
--- PoseModule.py
+++ PoseModule.py
@@ -5,13 +5,10 @@ import mediapipe as mp
 import time
 import math #angle을 구하기위해서 사용
 
-f = open("video_name.txt", 'r')
-m_name = f.read()
 
 #클래스 선언
 class poseDetector():
-
-    def __init__(self, mode=False, upBody=False,smooth=True,
+    def __init__(self, video_name, mode=False, upBody=False,smooth=True,
                  detectionCon=0.5, trackingCon=0.5): #파이썬 클래스, false를 주어 빠른 감지를함
         # static_image_mode = False,
         # upper_body_only=False,
@@ -22,7 +19,7 @@ class poseDetector():
         self.smooth=smooth
         self.detectionCon=detectionCon
         self.trackingCon=trackingCon
-
+        self.video_name = video_name
 
         self.mpDraw=mp.solutions.drawing_utils
         #포즈를 감지하여 여기에 쓸것임.
@@ -30,32 +27,27 @@ class poseDetector():
         self.pose=self.mpPose.Pose(self.mode,self.upBody,self.smooth, self.detectionCon
                                    ,self.trackingCon)
 
-        if not os.path.exists("./train"): # train/image폴더 생성
-            os.mkdir('./train')
-        if not os.path.exists("./train/image"): # train/image폴더 생성
-            os.mkdir('./train/image')
+        if not os.path.exists("./dataset/train/image"): # train/image폴더 생성
+            os.makedirs('./dataset/train/image')
 
-        #포즈찾기
+
+    #포즈찾기
     def findPose(self, img, black_img, index ,draw=True): #사용자는 그림을 그리시겠습니까 아니면 이미지에 표시하겠습니까
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         self.results = self.pose.process(imgRGB)
         self.mpDraw.draw_landmarks(black_img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
-        cv2.imwrite('train/image/'+ m_name + '_image%d.jpg'%index, black_img)
+        cv2.imwrite('./dataset/train/image/'+ self.video_name + '_image%d.jpg'%index, black_img)
         
         # print(results.pose_landmarks) #결과를 확인 x,y,z좌표 랜드마크를 확인
         if self.results.pose_landmarks:
             if draw:
                 self.mpDraw.draw_landmarks(img,self.results.pose_landmarks,self.mpPose.POSE_CONNECTIONS)
-                
                 #이미지의 좌표에 점을 생성,라인생성
                 #내가 원하는 랜드마크 번호 5를 원하면
-
         return img
 
     
-
-
     def findPosition(self,img,draw=True):
         self.lmList=[]
         if self.results.pose_landmarks: #결과가 사용가능한 경우
@@ -84,6 +76,7 @@ class poseDetector():
             angle += 360
             
         return angle     
+
 
     def drawPoint(self, img, p1, p2, p3, where, angle, draw=True):
         x1, y1 = self.lmList[p1][1:]
@@ -124,7 +117,11 @@ class poseDetector():
                         cv2.FONT_HERSHEY_PLAIN,2,CircleColor,2) #문자열로 바꿔야지 받아들여짐
         return
 
-def main():
+
+if __name__ == "__main__":
+    f = open("video_name.txt", 'r')
+    m_name = f.read()
+
     cap = cv2.VideoCapture(m_name + '.mp4')
     pTime = 0
     detector=poseDetector()
@@ -147,5 +144,3 @@ def main():
         cv2.imshow("Image", img)
         cv2.waitKey(1)  # 프레임을 줄이기위해서 10이면 초당 50 60프레임이 된다. 하지만 모델을 사용하면 그것에 대해 걱정할 필요가 없다.
     #모듈을 만들기 위해 필요한 과정
-if __name__ == "__main__":
-    main()
