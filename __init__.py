09ba936b2c1769398506f7ed8c1458df03cdb8ae
nudago <lukaslover@naver.com>
2021-11-27 14:48:41 +0900

    [ADD] missing init home_trainer

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
-__all__ = ["classification_model", "data_loader", "GUI", "utils"]
+__all__ = ["classification_model", "data_loader", "GUI", "utils", "home_trainer"]
 
 from . import classification_model
 from . import data_loader
 from . import GUI
 from . import utils
+from . import home_trainer
